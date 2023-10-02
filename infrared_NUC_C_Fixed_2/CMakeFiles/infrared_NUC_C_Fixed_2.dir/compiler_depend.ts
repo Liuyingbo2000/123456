83a5f9e1d9a1e21b3aafb5ce2344282ab41397b5
hhh <45894964+Liuyingbo2000@users.noreply.github.com>
2023-10-02 21:53:01 +0800

    Delete infrared_NUC_C_Fixed_2 directory

--- infrared_NUC_C_Fixed_2/CMakeFiles/infrared_NUC_C_Fixed_2.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for infrared_NUC_C_Fixed_2.
--- infrared_NUC_C_Fixed_2/CMakeFiles/infrared_NUC_C_Fixed_2.dir/main.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/infrared_NUC_C_Fixed_2.dir/main.c.o: \
- /home/hhh/Downloads/infrared_NUC_C_Fixed_2/main.c \
- /usr/include/stdc-predef.h \
- /home/hhh/Downloads/infrared_NUC_C_Fixed_2/main.h
